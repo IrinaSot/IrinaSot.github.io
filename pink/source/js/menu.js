017ba32e639b733c9f47e1848779894f93eda75a
IrinaSot <39626361+IrinaSot@users.noreply.github.com>
2020-01-17 13:17:27 +0700

    пинк3

---
+++ pink/source/js/menu.js
@@ -0,0 +1,19 @@
+    var navMain = document.querySelector('.main-nav');
+    var navToggle = document.querySelector('.main-nav__toggle');
+    var navMenu = document.querySelector('.page-header__top');
+
+    navMain.classList.remove('main-nav--nojs');
+
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+        navMenu.classList.remove('page-header__top-close');
+        navMenu.classList.add('page-header__top-open');
+      } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+        navMenu.classList.remove('page-header__top-open');
+        navMenu.classList.add('page-header__top-close');
+      }
+    });
